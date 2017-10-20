6a281bf954436319827f6841efd2e1ca5b4f5a7a
Garou <3296270793@qq.com>
2017-10-20 08:52:59 +0800

    10.20

--- app/src/main/java/com/example/administrator/login/LoginActivity.java
+++ app/src/main/java/com/example/administrator/login/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.administrator.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Administrator on 2017/10/17.
@@ -14,6 +17,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
